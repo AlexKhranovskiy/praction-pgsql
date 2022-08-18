7da65f9d046f7e18305fab94e45b28fdb1dbfdb4
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-08-18 12:45:17 +0300

    Realized pagination in branch 'with-paginator'

--- index.php
+++ index.php
@@ -2,6 +2,53 @@
 
 require_once('connect.php');
 
+session_start();
+
+function reader($msg, $pagination)
+{
+    $result = '';
+    $counter = 0;
+    try {
+        while ($message = $msg->fetch()) {
+            $result .= $message['id'] . ' ' . $message['message'] . ' (' . $message['created_at'] . ')' . PHP_EOL;
+            $counter++;
+            if ($counter == $pagination) {
+                $counter = 0;
+                yield $result;
+                $result = '';
+            }
+        }
+    } catch (PDOException $e) {
+        $result .= 'Query error' . PHP_EOL;
+        yield $result;
+    }
+}
+
+$query = 'SELECT * FROM messages';
+$msg = $pdo->query($query);
+
+$content = [];
+$_SESSION['page'] = 1;
+
+
+if (isset($_GET['page'])) {
+    $_SESSION['page'] = $_GET['page'];
+}
+
+$perviousPage = ($_SESSION['page']) - 1;
+$nextPage = ($_SESSION['page']) + 1;
+
+foreach (reader($msg, 4) as $item) {
+    $content[] = $item;
+}
+
+if ($perviousPage < 1) {
+    $perviousPage = 1;
+}
+if ($nextPage > count($content)) {
+    $nextPage = count($content);
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $query = 'INSERT INTO messages (message, created_at) VALUES (:message, :created_at)';
     $query = $pdo->prepare($query);
@@ -9,16 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         'message' => $_POST['message'],
         'created_at' => date('Y-m-d H:i:s')
     ]);
-}
-$query = 'SELECT * FROM messages';
-$msg = $pdo->query($query);
-
-try {
-    while ($message = $msg->fetch()) {
-        echo $message['id'] . ' ' . $message['message'] . ' ' . $message['created_at'] . '<br />';
-    }
-} catch (PDOException $e) {
-    echo 'Query error';
 }
 ?>
 
@@ -28,14 +65,17 @@ try {
     <title></title>
     <meta charset="utf-8">
 </head>
-<body>
-    
-    <form method='post'>
-        Write a message:<br /><br />
-        <textarea name='message'></textarea>
-        <br />
-        <input type='submit' value='Send'>
-    </form>
+<body>]
+<textarea rows='10' cols='80'> <?= $content[$_SESSION['page'] - 1] ?></textarea><br/>
+<a href="?page=<?= $perviousPage ?>"><--</a>&nbsp&nbsp<a href="?page=<?= $nextPage ?>">--></a><br/><br/>
+
+
+<form method='post'>
+    Write a message:<br/>
+    <textarea name='message' cols='30'></textarea>
+    <br/>
+    <input type='submit' value='Send'>
+</form>
 </body>
 </html>
 
